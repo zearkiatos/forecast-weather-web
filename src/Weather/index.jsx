6616f920f1cbb7946120baf559284a54b6110945
zearkiatos <caprilespe@outlook.com>
2022-05-20 16:30:47 -0400

    Add state param to the Weather Component

--- src/Weather/index.jsx
+++ src/Weather/index.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import PropTypes from "prop-types";
-import { WiCloud } from "react-icons/wi";
+import {
+  WiCloud,
+  WiDayCloudy,
+  WiDayFog,
+  WiDaySunny,
+  WiRain,
+  WiSnowy
+} from "react-icons/wi";
 import { IconContext } from "react-icons";
 
-const Weather = ({ temperature }) => (
+const stateByName = {
+  'CLOUD': WiCloud,
+  'CLOUDY': WiDayCloudy,
+  'FOG': WiDayFog,
+  'SUNNY': WiDaySunny,
+  'RAIN': WiRain,
+  'SNOWY': WiSnowy
+};
+
+const renderState = state => stateByName[state];
+
+const Weather = ({ temperature, state }) => (
   <div>
     <IconContext.Provider value={{ size: "5em" }}>
       <WiCloud />
