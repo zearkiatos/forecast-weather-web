142864aaa36ab4c914084c3e984e6b6611374929
zearkiatos <caprilespe@outlook.com>
2022-05-30 20:58:22 -0400

    Add Grid component CityList

--- src/components/CityList/index.jsx
+++ src/components/CityList/index.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Grid from "@mui/material/Grid";
 import CityInfo from "../CityInfo";
 import Weather from "../Weather";
-import WEATHERS from '../Weather/weathers';
+import WEATHERS from "../Weather/weathers";
 
 const renderCity = ({ city, country }) => (
   <li key={`${city}-${country}`}>
-    <CityInfo city={city} country={country} />
-    <Weather temperature={10} state={WEATHERS.SUNNY} />
+    <Grid container justify="center" alignItems="center">
+      <Grid item sm={8}>
+        <CityInfo city={city} country={country} />
+      </Grid>
+      <Grid item sm={4}>
+        <Weather temperature={10} state={WEATHERS.SUNNY} />
+      </Grid>
+    </Grid>
   </li>
 );
 
 const CityList = ({ cities }) => {
-  return (
-    <ul>
-      {cities.map(city => renderCity(city))}
-    </ul>
-  );
+  return <ul>{cities.map((city) => renderCity(city))}</ul>;
 };
 
 CityList.propTypes = {
