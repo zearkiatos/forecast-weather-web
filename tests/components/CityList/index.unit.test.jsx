32c72ad02ae9f091d3985a352dee9bc3cfc11be9
zearkiatos <caprilespe@outlook.com>
2022-06-03 15:58:33 -0400

    Adding event to CityList using TDD

--- tests/components/CityList/index.unit.test.jsx
+++ tests/components/CityList/index.unit.test.jsx
@@ -1,17 +1,38 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import React from "react";
-import { render } from "@testing-library/react";
-import '@testing-library/jest-dom/extend-expect';
-import CityList from '../../../src/components/CityList';
-import CityBuilder from '../../builders/cityBuilder';
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import CityList from "../../../src/components/CityList";
+import CityBuilder from "../../builders/cityBuilder";
 
 describe("Unit test suite for CityList component", () => {
-    test("Should render CityList component", async () => {
-        const cities = [new CityBuilder().build()];
-        const { findAllByRole } = render(<CityList cities={cities}/>);
+  test("Should render CityList component", async () => {
+    const cities = [new CityBuilder().build()];
+    const { findAllByRole } = render(<CityList cities={cities} />);
 
-        const cityListComponent = await findAllByRole("listitem");
+    const cityListComponent = await findAllByRole("listitem");
 
-        expect(cityListComponent).toHaveLength(1);
-    });
+    expect(cityListComponent).toHaveLength(1);
+  });
+
+  test("Should make click on the each item", async () => {
+    const clickOnItem = jest.fn();
+    const cities = [
+      new CityBuilder().build(),
+      new CityBuilder()
+        .withCity("Santiago de Chile")
+        .withCountry("Chile")
+        .build(),
+    ];
+
+    const { findAllByRole } = render(
+      <CityList cities={cities} onClickCity={clickOnItem} />
+    );
+
+    const items = await findAllByRole("listitem");
+    fireEvent.click(items[1]);
+
+    expect(clickOnItem).toHaveBeenCalled();
+    expect(clickOnItem).toHaveBeenCalledTimes(1);
+  });
 });
