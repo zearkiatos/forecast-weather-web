cac79777da27c458207208722bfb7718abefce42
zearkiatos <caprilespe@outlook.com>
2022-06-04 11:49:21 -0400

    Adding click event to the stories in the CityList component

--- src/components/CityList/CityList.stories.jsx
+++ src/components/CityList/CityList.stories.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import CityList from '.';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import CityList from ".";
 
 export default {
-    title: "CityList",
-    component: CityList
+  title: "CityList",
+  component: CityList,
 };
 
 const cities = [
-    {
-        city: "New York",
-        country: "United States"
-    },
-    {
-        city: "Santiago de Chile",
-        "country": "Chile"
-    },
-    {
-        city: "Puerto Cabello",
-        country: "Venezuela"
-    },
-    {
-        city: "Buenos Aires",
-        country: "Argentina"
-    },
-    {
-        city: "Bogota",
-        country: "Colombia"
-    },
-    {
-        city: "Madrid",
-        country: "Spain"
-    },
-    {
-        city: "Ciudad de Mexico",
-        country: "Mexico"
-    }
-]
+  {
+    city: "New York",
+    country: "United States",
+  },
+  {
+    city: "Santiago de Chile",
+    country: "Chile",
+  },
+  {
+    city: "Puerto Cabello",
+    country: "Venezuela",
+  },
+  {
+    city: "Buenos Aires",
+    country: "Argentina",
+  },
+  {
+    city: "Bogota",
+    country: "Colombia",
+  },
+  {
+    city: "Madrid",
+    country: "Spain",
+  },
+  {
+    city: "Ciudad de Mexico",
+    country: "Mexico",
+  },
+];
 
-export const CityListExample = () => <CityList cities={cities} />;
+export const CityListExample = () => (
+  <CityList cities={cities} onClickCity={action("Click in the city")} />
+);
