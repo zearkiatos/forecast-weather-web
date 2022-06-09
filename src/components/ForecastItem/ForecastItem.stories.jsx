8daa1aec9204fa2064dfb6c7f5e0ff8263b6d2cd
zearkiatos <caprilespe@outlook.com>
2022-06-08 21:57:37 -0400

    Creating the ForecastItem component

---
+++ src/components/ForecastItem/ForecastItem.stories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ForecastItem from ".";
+import WEATHERS from "../Weather/weathers";
+
+export default {
+  title: "ForecastItem",
+  component: ForecastItem,
+};
+
+export const MondaySunny = () => (
+  <ForecastItem
+    hour={10}
+    state={WEATHERS.SUNNY}
+    temperature={23}
+    weekDay="Monday"
+  />
+);
---
+++ src/components/ForecastItem/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+
+const ForecastItem = ({ weekDay, hour, state, temperature }) => {
+  return (
+    <Grid container direction="column" justify="center" alignItems="center">
+      <Grid item>
+        <Typography>{weekDay}</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>{hour}</Typography>
+      </Grid>
+      <Grid item>[Icon]{state}</Grid>
+      <Grid item>
+        <Typography>{temperature}°</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+ForecastItem.propTypes = {
+  weekDay: PropTypes.string.isRequired,
+  hour: PropTypes.number.isRequired,
+  state: PropTypes.string.isRequired,
+  temperature: PropTypes.number.isRequired,
+};
+
+export default ForecastItem;
