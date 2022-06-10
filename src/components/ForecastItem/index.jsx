0871fe71095abef40524e979627cffc22e3b39d4
zearkiatos <caprilespe@outlook.com>
2022-06-10 19:12:44 -0400

    Add icons to ForecastItem render

--- src/components/ForecastItem/index.jsx
+++ src/components/ForecastItem/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Typography } from "@mui/material";
+import {
+  WiCloud,
+  WiDayCloudy,
+  WiDayFog,
+  WiDaySunny,
+  WiRain,
+  WiSnow,
+} from "react-icons/wi";
+import { IconContext } from "react-icons";
 import Grid from "@mui/material/Grid";
+import WEATHERS from "../Weather/weathers";
+
+const stateByName = {
+  CLOUD: <WiCloud />,
+  CLOUDY: <WiDayCloudy />,
+  FOG: <WiDayFog />,
+  SUNNY: <WiDaySunny />,
+  RAIN: <WiRain />,
+  SNOW: <WiSnow />,
+};
+
+const renderState = (state) => stateByName[state] || <WiDaySunny />;
 
 const ForecastItem = ({ weekDay, hour, state, temperature }) => {
   return (
@@ -12,7 +33,11 @@ const ForecastItem = ({ weekDay, hour, state, temperature }) => {
       <Grid item>
         <Typography>{hour}</Typography>
       </Grid>
-      <Grid item>[Icon]{state}</Grid>
+      <Grid item>
+        <IconContext.Provider value={{ size: "5em" }}>
+          {renderState(state)}
+        </IconContext.Provider>
+      </Grid>
       <Grid item>
         <Typography>{temperature}°</Typography>
       </Grid>
@@ -20,10 +45,12 @@ const ForecastItem = ({ weekDay, hour, state, temperature }) => {
   );
 };
 
+const stateValidValues = Object.values(WEATHERS);
+
 ForecastItem.propTypes = {
   weekDay: PropTypes.string.isRequired,
   hour: PropTypes.number.isRequired,
-  state: PropTypes.string.isRequired,
+  state: PropTypes.oneOf(stateValidValues).isRequired,
   temperature: PropTypes.number.isRequired,
 };
 
