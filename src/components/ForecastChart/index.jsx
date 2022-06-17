d1255002b7a87ab5b42f635d5f0cf3d8d4da1459
zearkiatos <caprilespe@outlook.com>
2022-06-17 16:03:02 -0400

    Adding validation of ForecastChart

--- src/components/ForecastChart/index.jsx
+++ src/components/ForecastChart/index.jsx
@@ -7,6 +7,14 @@ const ForecastChart = props => {
   )
 }
 
-ForecastChart.propTypes = {}
+ForecastChart.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            dayHour: PropTypes.string.isRequired,
+            min: PropTypes.number.isRequired,
+            max: PropTypes.number.isRequired
+        })
+    ).isRequired
+}
 
 export default ForecastChart
