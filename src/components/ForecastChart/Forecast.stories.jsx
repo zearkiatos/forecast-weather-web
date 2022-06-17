08325999c3c6d4cb82a5951b79c338f4d1dcf421
zearkiatos <caprilespe@outlook.com>
2022-06-17 15:54:18 -0400

    Creating the structure of ForecastChart

---
+++ src/components/ForecastChart/Forecast.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ForecastChart from ".";
+
+export default {
+  title: "ForecastChart",
+  component: ForecastChart,
+};
+
+export const ForecastChartExample = () => (<ForecastChart />);
