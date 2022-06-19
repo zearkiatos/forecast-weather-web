bfcacc005846835472f44a5cd2f1afedcf976283
zearkiatos <caprilespe@outlook.com>
2022-06-19 11:53:24 -0400

    Creating the pages

---
+++ src/pages/City/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const City = (props) => {
+  return <div>City Page</div>;
+};
+
+City.propTypes = {};
+
+export default City;
---
+++ src/pages/Welcome/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Welcome = (props) => {
+  return <div>Welcome Page</div>;
+};
+
+Welcome.propTypes = {};
+
+export default Welcome;
