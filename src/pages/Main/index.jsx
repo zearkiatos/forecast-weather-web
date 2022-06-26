024de89bdf2fb8083f0259a88adea6d6451903bc
zearkiatos <caprilespe@outlook.com>
2022-06-26 14:32:07 -0400

    Adding CityList in Main Page

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,13 +1,47 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import CityList from "../../components/CityList";
 
+const citiesMock = [
+  {
+    city: "New York",
+    country: "United States",
+  },
+  {
+    city: "Santiago de Chile",
+    country: "Chile",
+  },
+  {
+    city: "Puerto Cabello",
+    country: "Venezuela",
+  },
+  {
+    city: "Buenos Aires",
+    country: "Argentina",
+  },
+  {
+    city: "Bogota",
+    country: "Colombia",
+  },
+  {
+    city: "Madrid",
+    country: "Spain",
+  },
+  {
+    city: "Ciudad de Mexico",
+    country: "Mexico",
+  },
+];
 const Main = () => {
   const navigate = useNavigate();
-  const onClickHandler = () => navigate("/city");
+
+  const onClickHandler = () => {
+    navigate("/city");
+  };
   return (
     <div>
-      Main Page
-      <button onClick={onClickHandler}>Go to City Page</button>
+      <h2>City list</h2>
+      <CityList cities={citiesMock} onClickCity={onClickHandler} />
     </div>
   );
 };
