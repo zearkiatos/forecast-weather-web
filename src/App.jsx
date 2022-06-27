9b2872a43e82328ae6b24091f85c5658933c1cb9
zearkiatos <caprilespe@outlook.com>
2022-06-27 12:06:52 -0400

    Center App content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Grid from "@mui/material/Grid";
 import Welcome from "./pages/Welcome";
 import Main from "./pages/Main";
 import City from "./pages/City";
@@ -7,17 +8,19 @@ import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/city" element={<City />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Grid container justifyContent="center" direction="row">
+      <Grid item sm={10}>
+        <h1>App</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/city" element={<City />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Grid>
+    </Grid>
   );
 };
 
