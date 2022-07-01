a4bd9507deb51b06484deef845d931ed950dd6ac
zearkiatos <caprilespe@outlook.com>
2022-07-01 16:07:46 -0400

    Adding ResponsiveContainer to ForecastChart

--- src/components/ForecastChart/index.jsx
+++ src/components/ForecastChart/index.jsx
@@ -8,13 +8,12 @@ import {
   YAxis,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 const ForecastChart = ({ data }) => {
   return (
-    <div>
+    <ResponsiveContainer height={250} width={"95%"}>
       <LineChart
-        height={250}
-        width={700}
         margin={{ top: 20, bottom: 20, left: 5, right: 5 }}
         data={data}
       >
@@ -26,7 +25,7 @@ const ForecastChart = ({ data }) => {
         <Line type="monotone" dataKey="max" stroke="#FF0000"></Line>
         <Line type="monotone" dataKey="min" stroke="#0000FF"></Line>
       </LineChart>
-    </div>
+    </ResponsiveContainer>
   );
 };
 
