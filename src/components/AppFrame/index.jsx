6836e246233c78d5aae789a3bb88b85244c757a7
zearkiatos <caprilespe@outlook.com>
2022-07-04 21:41:23 -0400

    Creating the AppFrame Component

---
+++ src/components/AppFrame/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AppFrame = (props) => {
+  return <div>AppFrame</div>;
+};
+
+AppFrame.propTypes = {};
+
+export default AppFrame;
