0b4d344f50cbc02d315752ae182002f4a6e0aa8c
zearkiatos <caprilespe@outlook.com>
2022-07-11 21:07:42 -0400

    Using the useRef

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 
 const Welcome = ({ children }) => {
-  return <div>Welcome</div>;
+  const myRefDiv = useRef(null);
+  console.log("myRefDiv.current", myRefDiv.current);
+  return <div ref={myRefDiv}>Welcome</div>;
 };
 
 Welcome.propTypes = {
