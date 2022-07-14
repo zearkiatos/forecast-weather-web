0ba4a16f7dc227cbfee3c58fe1603d323ca5ac54
zearkiatos <caprilespe@outlook.com>
2022-07-13 22:25:28 -0400

    Using useEffect

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const Welcome = ({ children }) => {
   const myRefDiv = useRef(null);
-  console.log("myRefDiv.current", myRefDiv.current);
+  useEffect(() => {
+    console.log(`myRefDiv.current ${myRefDiv.current}`);
+  });
   return <div ref={myRefDiv}>Welcome</div>;
 };
 
