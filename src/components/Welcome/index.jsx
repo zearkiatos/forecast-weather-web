6d288222a70b7bb0fadc68e67c9bb440454aa7f6
zearkiatos <caprilespe@outlook.com>
2022-07-15 16:42:31 -0400

    Interaction of useState and useEffect

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,11 +1,16 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const Welcome = ({ children }) => {
   const myRefDiv = useRef(null);
+  const [vanta, setVanta] = useState(0);
   useEffect(() => {
     console.log(`myRefDiv.current ${myRefDiv.current}`);
-  });
+    if (!vanta) {
+      setVanta(1);
+      console.log("Vanta was initialized");
+    }
+  }, []);
   return <div ref={myRefDiv}>Welcome</div>;
 };
 
