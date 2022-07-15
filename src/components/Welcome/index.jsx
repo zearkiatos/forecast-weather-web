8b29cc6c4346f122805cd80715b4f7fdd9ac497c
zearkiatos <caprilespe@outlook.com>
2022-07-15 17:05:36 -0400

    Activate clouds effect vanta

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,14 +1,19 @@
 import React, { useRef, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import Clouds from "vanta/dist/vanta.clouds.min";
+import * as THREE from "three";
 
 const Welcome = ({ children }) => {
   const myRefDiv = useRef(null);
-  const [vanta, setVanta] = useState(0);
+  const [vanta, setVanta] = useState(null);
   useEffect(() => {
-    console.log(`myRefDiv.current ${myRefDiv.current}`);
     if (!vanta) {
-      setVanta(1);
-      console.log("Vanta was initialized");
+      setVanta(
+        Clouds({
+          THREE,
+          el: myRefDiv.current,
+        })
+      );
     }
   }, [vanta]);
   return <div ref={myRefDiv}>Welcome</div>;
