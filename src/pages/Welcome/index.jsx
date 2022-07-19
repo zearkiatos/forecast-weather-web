605b79d4d33e92023e624042d1f9516d8b428cde
zearkiatos <caprilespe@outlook.com>
2022-07-18 22:37:07 -0400

    Improvement the welcome page

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import { IconContext } from "react-icons";
+import { WiDaySunny } from "react-icons/wi";
 import WelcomeScreen from "../../components/Welcome";
+
 const Welcome = () => {
   return (
     <WelcomeScreen>
@@ -11,7 +14,30 @@ const Welcome = () => {
         justifyContent="center"
       >
         <div className="highlight">
-
+          <Grid
+            item
+            container
+            xs={12}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <IconContext.Provider value={{ size: "6em" }}>
+                <WiDaySunny />
+              </IconContext.Provider>
+            </Grid>
+            <Grid
+              item
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Typography variant="h4" color="inherit">
+                Weather Application
+              </Typography>
+            </Grid>
+          </Grid>
         </div>
       </Grid>
     </WelcomeScreen>
