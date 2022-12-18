588a1a5e778f1467264c07286c6b1361404ffcd9
zearkiatos <caprilespe@outlook.com>
2022-12-18 17:58:29 -0300

    Class component basic structure

---
+++ src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ErrorBoundary from ".";
+
+export default {
+  title: "ErrorBoundary",
+  component: ErrorBoundary,
+};
+
+export const ErrorBoundaryExample = () => <ErrorBoundary />;
---
+++ src/generic/ErrorBoundary/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  render() {
+    return <h1>ErrorBoundary</h1>;
+  }
+}
+
+export default ErrorBoundary;
