79898e55d03d71ac5282eef6df0dec1fdd66a176
zearkiatos <caprilespe@outlook.com>
2022-12-22 22:22:46 -0300

    Classes modification states

--- src/generic/ErrorBoundary/index.jsx
+++ src/generic/ErrorBoundary/index.jsx
@@ -4,19 +4,27 @@ class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activated: false
-    }
+      activated: false,
+    };
   }
   isActivated = () => {
     const { activated } = this.state;
     return activated ? "Activated" : "Not Activated";
   };
+  onClickHandler = () => {
+    this.setState({
+      activated: true,
+    });
+  };
   render() {
     return (
-      <h1>
-        ErrorBoundary {this.props.greet}
-        {` ${this.isActivated()}`}
-      </h1>
+      <div>
+        <button onClick={this.onClickHandler}>Activate</button>
+        <h1>
+          ErrorBoundary {this.props.greet}
+          {` ${this.isActivated()}`}
+        </h1>
+      </div>
     );
   }
 }
