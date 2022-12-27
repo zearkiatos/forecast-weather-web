e2679bb44d31789c1e1991d0e53e5dd442099400
zearkiatos <caprilespe@outlook.com>
2022-12-26 23:13:11 -0300

    Presentation of dunction of ErrorBoundary

--- src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
+++ src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -6,6 +6,24 @@ export default {
   component: ErrorBoundary,
 };
 
+const ComponentWithoutError = () => <h1>Without Error</h1>;
+
+const prop = undefined;
+const ComponentWithError = () => <h1>{prop.hello}</h1>;
+
+
 export const ErrorBoundaryExample = () => (
   <ErrorBoundary greet="Hello" />
+);
+
+export const ErrorBoundaryWithErrorExample = () => (
+  <ErrorBoundary>
+    <ComponentWithError />
+  </ErrorBoundary>
+);
+
+export const ErrorBoundaryWithoutError = () => (
+  <ErrorBoundary>
+    <ComponentWithoutError />
+  </ErrorBoundary>
 );
