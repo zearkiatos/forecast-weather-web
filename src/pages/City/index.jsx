2e5ce37b309711c2038d0fffa4f1cc621bf5e312
zearkiatos <caprilespe@outlook.com>
2023-01-20 17:10:00 -0300

    Extract city page hook from city page

--- src/pages/City/index.jsx
+++ src/pages/City/index.jsx
@@ -1,9 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Grid from "@mui/material/Grid";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import moment from "moment";
-import convertUnits from "convert-units";
 import "moment/locale/es";
 import CityInfo from "../../components/CityInfo";
 import Weather from "../../components/Weather";
@@ -11,64 +7,7 @@ import WeatherDetails from "../../components/WeatherDetails";
 import ForecastChart from "../../components/ForecastChart";
 import Forecast from "../../components/Forecast";
 import AppFrame from "../../components/AppFrame";
-import config from "../../config";
-
-const useCityPage = () => {
-  const [chartData, setChartData] = useState(null);
-  const [forecastItemList, setForecastItemList] = useState(null);
-  const { city, countryCode } = useParams();
-  const getForecast = async () => {
-    const toCelsius = (temp) =>
-      Number(convertUnits(temp).from("K").to("C").toFixed());
-    const url = `${config.OPEN_WEATHER_MAP.API_BASE_URL}/data/${config.OPEN_WEATHER_MAP.VERSION}/forecast?q=${city},${countryCode}&appid=${config.OPEN_WEATHER_MAP.API_KEY}`;
-    try {
-      const { data } = await axios.get(url);
-      console.log(data);
-      const daysAhead = [0, 1, 2, 3, 4, 5];
-      const days = daysAhead.map((day) => moment().add(day, "d"));
-      const date = days
-        .map((day) => {
-          const tempObjectArray = data.list.filter((item) => {
-            const dayOfYear = moment.unix(item.dt).dayOfYear();
-            return dayOfYear === day.dayOfYear();
-          });
-          console.log("dayOfYear", day.dayOfYear());
-          console.log("tempObjectArray", tempObjectArray);
-          const temps = tempObjectArray.map((item) => item.main.temp);
-
-          console.log(temps);
-          return {
-            dayHour: day.format("ddd"),
-            min: toCelsius(Math.min(...temps)),
-            max: toCelsius(Math.max(...temps)),
-            hasTemperature: temps.length > 0,
-          };
-        })
-        .filter((item) => item.hasTemperature);
-      setChartData(date);
-      const interval = [4, 8, 12, 16, 20, 24];
-
-      const forecastItemList = data.list
-        .filter((item, index) => interval.includes(index))
-        .map((item) => ({
-          hour: moment.unix(item.dt).hour(),
-          weekDay: moment.unix(item.dt).format("dddd"),
-          state: item.weather[0].main.toUpperCase(),
-          humidity: item.main.humidity,
-          wind: item.wind.speed,
-          temperature: toCelsius(item.main.temp),
-        }));
-      setForecastItemList(forecastItemList);
-    } catch (ex) {
-      console.error(`Error: ${ex.message}`);
-    }
-  };
-  useEffect(() => {
-    getForecast();
-  }, [city, countryCode]);
-
-  return { city, chartData, forecastItemList, countryCode };
-};
+import useCityPage from "../../hooks/useCityPage";
 
 const City = () => {
   const { city, chartData, forecastItemList, countryCode } = useCityPage();
