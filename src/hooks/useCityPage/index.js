f29ce68d0fed91ffe301af1ea2f02ce56a634d49
zearkiatos <caprilespe@outlook.com>
2023-06-02 19:30:33 -0400

    Function transformation getChartData

--- src/hooks/useCityPage/index.js
+++ src/hooks/useCityPage/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import moment from "moment";
 import { getForecastUrl } from "../../utils/constants/urls";
 import { toCelsius } from "../../utils/constants/convertion";
+import getChartData from "../../utils/transform/getChartData";
 
 const useCityPage = () => {
   const [chartData, setChartData] = useState(null);
@@ -13,24 +14,7 @@ const useCityPage = () => {
     const url = getForecastUrl({ city, countryCode });
     try {
       const { data } = await axios.get(url);
-      const daysAhead = [0, 1, 2, 3, 4, 5];
-      const days = daysAhead.map((day) => moment().add(day, "d"));
-      const date = days
-        .map((day) => {
-          const tempObjectArray = data.list.filter((item) => {
-            const dayOfYear = moment.unix(item.dt).dayOfYear();
-            return dayOfYear === day.dayOfYear();
-          });
-          const temps = tempObjectArray.map((item) => item.main.temp);
-
-          return {
-            dayHour: day.format("ddd"),
-            min: toCelsius(Math.min(...temps)),
-            max: toCelsius(Math.max(...temps)),
-            hasTemperature: temps.length > 0,
-          };
-        })
-        .filter((item) => item.hasTemperature);
+      const date = getChartData(data);
       setChartData(date);
       const interval = [4, 8, 12, 16, 20, 24];
 
