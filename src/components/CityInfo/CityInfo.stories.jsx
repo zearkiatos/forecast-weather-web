ade85aa63a51c309e8a61ae2612308dbed645a02
zearkiatos <caprilespe@outlook.com>
2023-07-15 11:18:04 -0400

    Update storybook 6

--- src/components/CityInfo/CityInfo.stories.jsx
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import 'typeface-roboto';
-import CityInfo from '.';
+import React from "react";
+import "typeface-roboto";
+import CityInfo from ".";
 
 export default {
-    title: 'CityInfo',
-    component: CityInfo
+  title: "CityInfo",
+  component: CityInfo,
 };
 
-export const CityExample = () => <CityInfo city="Puerto Cabello" country="Venezuela"></CityInfo>
+export const CityExample = (args) => <CityInfo {...args}></CityInfo>;
+
+CityExample.args = {
+  city: "Puerto Cabello",
+  country: "Venezuela",
+};
